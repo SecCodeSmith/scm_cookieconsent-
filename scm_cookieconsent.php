70de3556c23ffeac32732172ef52bd2e313ffb89
SecCodeSmith <admin@seccodesmith.pl>
2026-07-22 10:35:55 +0200

    feat: Bump version to 1.6.7 and adjust cookie banner width

--- scm_cookieconsent.php
+++ scm_cookieconsent.php
@@ -28,7 +28,7 @@ class Scm_Cookieconsent extends Module
     {
         $this->name          = 'scm_cookieconsent';
         $this->tab           = 'front_office_features';
-        $this->version       = '1.6.6';
+        $this->version       = '1.6.7';
         $this->author        = 'SecCodeSmith';
         $this->need_instance = 0;
         $this->bootstrap     = true;
